58ab56a3dfb5546142d600f573a0ff0b54164c72
ma914372 <madhurima.rima20@gmail.com>
2025-01-25 22:14:19 +0100

    Modified

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 # Outputs
 output "master_node_ip" {
-  value = aws_instance.kubernetes_master.public_ip
+  value = aws_instance.kubernetes_master[*].public_ip
 }
 
 output "worker_node_ips" {
@@ -11,6 +11,10 @@ output "ansible_node_ip" {
   value = aws_instance.ansible_node.public_ip
 }
 
-output "elb_dns_name" {
-  value = aws_elb.kubernetes_elb.dns_name
+#output "elb_dns_name" {
+  #value = aws_elb.kubernetes_elb.dns_name
+#}
+output "alb_dns_name" {
+  value = aws_lb.kubernetes_elb.dns_name
+  description = "The DNS name of the ALB"
 }
