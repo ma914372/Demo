57fb1b0357abfb00b3249b51a1086748ae2c6786
ma914372 <45353975+ma914372@users.noreply.github.com>
2025-01-25 18:02:04 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,26 +10,22 @@ variable "region" {
     default = "us-east-1"
 }
 
-variable "subnet_cidr" {
-    description = "CIDR for subnet"
-    default = "10.0.1.0/24"
-}
 variable "subnet_cidr_kubernetes_a" {
   description = "CIDR block for Kubernetes Subnet A"
   type        = string
-  default = "10.0.2.0/24"
+  default = "10.0.1.0/24"
 }
 
 variable "subnet_cidr_kubernetes_b" {
   description = "CIDR block for Kubernetes Subnet B"
   type        = string
-  default = "10.0.3.0/24"
+  default = "10.0.2.0/24"
 }
 
 variable "subnet_cidr_ansible" {
   description = "CIDR block for Ansible Subnet"
   type        = string
-  default = "10.0.5.0/24"
+  default = "10.0.3.0/24"
 }
 
 variable "instance_type" {
