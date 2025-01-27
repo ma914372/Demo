00fc207651b158928f0e9d116b1484e01efa88ac
ma914372 <45353975+ma914372@users.noreply.github.com>
2025-01-27 21:38:07 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -14,14 +14,32 @@
 #output "elb_dns_name" {
   #value = aws_elb.kubernetes_elb.dns_name
 #}
+#output "master_node_ip" {
+ # value = { for idx, ip in aws_instance.kubernetes_master : "master${idx + 1}" => ip.public_ip }
+#}
+
+#output "worker_node_ips" {
+ # value = { for idx, ip in aws_instance.kubernetes_worker_nodes : "worker${idx + 1}" => ip.public_ip }
+#}
+
+#output "ansible_node_ip" {
+ # value = { "ansible_node" = aws_instance.ansible_node.public_ip }
+#}
+
+
+
+
+
 output "master_node_ip" {
-  value = { for idx, ip in aws_instance.kubernetes_master : "master${idx + 1}" => ip.public_ip }
+  value = join(",", [for ip in aws_instance.kubernetes_master : ip.public_ip])
 }
 
 output "worker_node_ips" {
-  value = { for idx, ip in aws_instance.kubernetes_worker_nodes : "worker${idx + 1}" => ip.public_ip }
+  value = join(",", [for ip in aws_instance.kubernetes_worker_nodes : ip.public_ip])
 }
 
 output "ansible_node_ip" {
-  value = { "ansible_node" = aws_instance.ansible_node.public_ip }
+  value = aws_instance.ansible_node.public_ip
 }
+
+
