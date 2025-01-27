39f713191f0c95fc6648d2915561a69557f1af63
ma914372 <madhurima.rima20@gmail.com>
2025-01-27 14:13:43 +0100

    Modified

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,27 @@
 # Outputs
+#output "master_node_ip" {
+  #value = aws_instance.kubernetes_master[*].public_ip
+#}
+
+#output "worker_node_ips" {
+ # value = aws_instance.kubernetes_worker_nodes[*].public_ip
+#}
+
+#output "ansible_node_ip" {
+ # value = aws_instance.ansible_node.public_ip
+#}
+
+#output "elb_dns_name" {
+  #value = aws_elb.kubernetes_elb.dns_name
+#}
 output "master_node_ip" {
-  value = aws_instance.kubernetes_master[*].public_ip
+  value = join(",", aws_instance.kubernetes_master[*].public_ip)
 }
 
 output "worker_node_ips" {
-  value = aws_instance.kubernetes_worker_nodes[*].public_ip
+  value = join(",", aws_instance.kubernetes_worker_nodes[*].public_ip)
 }
 
 output "ansible_node_ip" {
   value = aws_instance.ansible_node.public_ip
 }
-
-#output "elb_dns_name" {
-  #value = aws_elb.kubernetes_elb.dns_name
-#}
