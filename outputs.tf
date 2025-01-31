e7d1e94752aa4df9aa1d170f1f4d28ba196f1667
ma914372 <45353975+ma914372@users.noreply.github.com>
2025-01-31 11:39:14 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -31,7 +31,7 @@
 
 
 output "master_node_ip" {
-  value = join(",", [for ip in aws_instance.kubernetes_master : ip.public_ip])
+  value = aws_instance.kubernetes_master.public_ip
 }
 
 output "worker_node_ips" {
@@ -40,6 +40,13 @@ output "worker_node_ips" {
 
 output "ansible_node_ip" {
   value = aws_instance.ansible_node.public_ip
+}
+output "master_private_ip" {
+  value = aws_instance.kubernetes_master.private_ip
+}
+
+output "worker_private_ips" {
+  value = join(",", [for ip in aws_instance.kubernetes_worker_nodes : ip.private_ip])
 }
 
 
