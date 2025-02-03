e815ccc49bcf70232e80fa255d0f0c3e4c5d4bb3
ma914372 <45353975+ma914372@users.noreply.github.com>
2025-02-03 11:17:57 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "my-demo-bucket2025"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+
+  }
+}
 provider "aws" {
     region = "${var.region}"
 }
