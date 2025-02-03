9909b29f2891a3a59c3365ab1f676f299f063124
ma914372 <45353975+ma914372@users.noreply.github.com>
2025-02-03 14:32:36 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,6 +32,16 @@ resource "aws_subnet" "kubernetes_subnet" {
 }
 
 
+resource "aws_subnet" "ansible_subnet" {
+  vpc_id                  = aws_vpc.demo_vpc.id
+  cidr_block              = "${var.subnet_cidr_ansible}"
+  availability_zone       =  "us-east-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Ansible-Subnet"
+  }
+}
+
 resource "aws_internet_gateway" "demo_igw" {
   vpc_id = aws_vpc.demo_vpc.id
 
@@ -54,20 +64,11 @@ resource "aws_route_table_association" "subnet_association" {
   route_table_id = aws_route_table.demo_route_table.id
 }
 
-
-resource "aws_subnet" "ansible_subnet" {
-  vpc_id                  = aws_vpc.demo_vpc.id
-  cidr_block              = "${var.subnet_cidr_ansible}"
-  availability_zone       =  "us-east-1b"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "Ansible-Subnet"
-  }
-}
 resource "aws_route_table_association" "ansible_subnet_association" {
-    subnet_id = aws_subnet.ansible_subnet.id
-    route_table_id = aws_route_table.demo_route_table.id
+  subnet_id      = aws_subnet.ansible_subnet.id
+  route_table_id = aws_route_table.demo_route_table.id
 }
+
 # Security Group for Master Nodes
 resource "aws_security_group" "kubernetes_master_sg" {
   vpc_id = aws_vpc.demo_vpc.id
